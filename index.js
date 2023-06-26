18d277c10e842286736ab6cb6e6f360a7c42eb77
wachirarobbinson <ngirigacharobbinson@gmail.com>
2023-06-26 12:04:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
